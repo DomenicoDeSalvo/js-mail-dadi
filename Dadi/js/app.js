ccc8e78b84d356717ff582a21b45b27168530f4f
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-03-07 16:35:19 +0100

    Added to the exercise "Dadi": Pseudocode and layout.

---
+++ Dadi/js/app.js
@@ -0,0 +1,7 @@
+//Fare sì che, cliccando sul bottone "Genera", vengano selezionati due numeri casuali da 1 a 6, il primo verrrà assegnato all'utente, il secondo al computer. Chi avrà il numero più alto vincerà la partita.
+
+//1 Rendere responsive il tasto "Genera".
+
+//2 Generare i due numeri e assegnarli.
+
+//3 Generare una risposta per l'utente. 
