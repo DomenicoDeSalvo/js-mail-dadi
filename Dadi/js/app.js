72cb15274fc8aeee8dd3fc8db37c7558f20c8472
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-03-07 20:42:23 +0100

    fixes

--- Dadi/js/app.js
+++ Dadi/js/app.js
@@ -1,7 +1,7 @@
 //Fare sì che, cliccando sul bottone "Genera", vengano selezionati due numeri casuali da 1 a 6, il primo verrrà assegnato all'utente, il secondo al computer. Chi avrà il numero più alto vincerà la partita.
 
 //Acquisire il tasto "Genera".
-const buttonElement = document.querySelector('button'); //Element | Null
+const buttonElement = document.querySelector('.button'); //Element | Null
 //Renderlo responsive.
 buttonElement.addEventListener('click', function() {
 
