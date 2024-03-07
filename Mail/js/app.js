80c345e8cd464b0ad9bd8bc6127887667115265b
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-03-07 20:42:16 +0100

    Added validation code

---
+++ Mail/js/app.js
@@ -0,0 +1,43 @@
+//Acquisire la mail dell'utente, confrontarla con una lista e restituire un messaggio di risposta.
+
+//Acquisizione mail.
+const inputMailElement = document.getElementById('mail'); //Element | Null
+
+//Acquisizione bottone submit.
+const submitElement = document.querySelector('.button'); //Element | Null
+
+//Lista delle email autorizzate
+
+const mailList = ['pincopallo@mail.com', 'tizio@mail.com', 'caio@mail.com', 'sempronio@mail.com', 'giorgio_mastrota@mail.com' ]//String
+
+//Cliccando il bottone si avvierà l'algoritmo. 
+submitElement.addEventListener('click', function(){
+    let userMail = inputMailElement.value; //string
+
+    //Validazione della mail.
+    if(userMail != ('')){
+        
+        //Creazione variabile di controllo.
+        let authorized  = false;
+        
+        //Creazione ciclo di controllo della lista delle mail autorizzate.
+        for (let i = 0; i < mailList.length; i++) {
+            const authorizedMail = mailList[i];
+            
+            //Controllo della corrispondenza tra la mail dell'utente e le mail della lista.
+            if(userMail === authorizedMail){
+                authorized = true;
+            }    
+        }
+        
+        //Risposta all'utente.
+        if(authorized === true){
+            console.log('La mail è autorizzata ad accedere')
+        } else {
+            alert('Inserire una mail autorizzata')
+            console.log('La mail non è autorizzata ad accedere')
+        }
+    } else {
+        alert ('Inserire una mail')
+    }
+})
