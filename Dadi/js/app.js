fb8a1c1243dff9cc3b7036a53947de3452fb122f
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-03-07 17:22:37 +0100

    Adding code to js

--- Dadi/js/app.js
+++ Dadi/js/app.js
@@ -1,7 +1,29 @@
 //Fare sì che, cliccando sul bottone "Genera", vengano selezionati due numeri casuali da 1 a 6, il primo verrrà assegnato all'utente, il secondo al computer. Chi avrà il numero più alto vincerà la partita.
 
-//1 Rendere responsive il tasto "Genera".
+//Acquisire il tasto "Genera".
+const buttonElement = document.querySelector('button'); //Element | Null
+//Renderlo responsive.
+buttonElement.addEventListener('click', function() {
 
-//2 Generare i due numeri e assegnarli.
+    //Dichiarare i giocatori e assegnazione dei numeri.
+    let user = Math.floor(Math.random() * (6 - 1 + 1) + 1); //Number
+    computer = Math.floor(Math.random() * (6 - 1 + 1) + 1); //Number
 
-//3 Generare una risposta per l'utente. 
+    // Generare una risposta per l'utente.
+    // Acquisire elemento nel DOM in cui generare la risposta.
+    let resultSectionElement = document.querySelector('result'); //Element | Null
+    // Mostrare il risultato.
+    // Creare elemento nella sezione risultato.
+    let divResultElement = document.createElement('div'); //Element | Null
+    //Mostrare risultato
+    resultSectionElement.append(divResultElement);
+    divResultElement.innerHTML = 'Tu' + user + 'Computer' + computer;
+    //resultElement
+    //if(user > computer) {
+        
+
+    //}
+
+
+});
+
